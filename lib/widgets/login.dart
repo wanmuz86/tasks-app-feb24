6f1cb0461485d3e8ac5c74b6cf0a55e88435c217
Wan Muzaffar <wanmuz@Wans-MacBook-Air.local>
2024-02-07 15:58:23 +0800

    add login page without saving token

--- lib/widgets/login.dart
+++ lib/widgets/login.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:tasks_api/widgets/register.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'home.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  var emailEditingController = TextEditingController();
+  var passwordEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -15,16 +17,46 @@ class LoginPage extends StatelessWidget {
         child: Column(
           children: [
             TextField(decoration: InputDecoration(hintText: "Enter email"),
-            keyboardType: TextInputType.emailAddress,),
+            keyboardType: TextInputType.emailAddress, controller: emailEditingController,),
             TextField(decoration: InputDecoration(hintText: "Enter password",),
-              obscureText: true,),
-            ElevatedButton(onPressed: (){}, child: Text("Login")),
+              obscureText: true, controller: passwordEditingController,),
+            ElevatedButton(onPressed: (){
+              loginUser(emailEditingController.text, passwordEditingController.text)
+                  .then((response) {
+                 if (response.statusCode == 200){
+                   if (jsonDecode(response.body)["token"] != null){
+                     var token = jsonDecode(response.body)["token"];
+                     SnackBar snackbar = SnackBar(content: Text("Successfully logged in"));
+                     ScaffoldMessenger.of(context).showSnackBar(snackbar);
+                     Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
+                   }
+                   
+                 }
+                 else {
+                   SnackBar snackbar = SnackBar(content: Text("Something is wrong"));
+                   ScaffoldMessenger.of(context).showSnackBar(snackbar);
+                 }
+              });
+
+            }, child: Text("Login")),
             TextButton(onPressed: (){
               Navigator.push(context, MaterialPageRoute(builder: (context)=> RegisterPage()));
             }, child: Text("No account? Register now!"))
           ],
         ),
       ),
+    );
+  }
+  Future<http.Response> loginUser(String email, String password) {
+    return http.post(
+      Uri.parse('https://employee-rest-api-vs0n.onrender.com/api/login'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'email': email,
+        'password':password
+      }),
     );
   }
 
